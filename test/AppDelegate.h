135fd75a78c43937cc52b779c4140177c9496480
Owner <Owner@Hub.local>
2013-11-20 20:36:33 -0500

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Owner on 11/20/13.
+//  Copyright (c) 2013 Owner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
